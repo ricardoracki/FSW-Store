582ca42acecff379159cd002ecef2ff0e8cecc87
Ricardo Racki <ricardo_racki@hotmail.com>
2023-10-18 19:52:27 -0300

    feat: ensure user goes to catalog page on click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -10,11 +10,18 @@ import {
   PercentIcon,
   ShoppingCartIcon,
 } from "lucide-react";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTrigger,
+} from "./sheet";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Avatar } from "./avatar";
 import { AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 import { Separator } from "@radix-ui/react-separator";
+import Link from "next/link";
 
 function Header() {
   const { status, data } = useSession();
@@ -91,10 +98,17 @@ function Header() {
               Ofertas
             </Button>
 
-            <Button variant={"outline"} className="w-full justify-start gap-2">
-              <ListOrderedIcon size={16} />
-              Catálogo
-            </Button>
+            <SheetClose asChild>
+              <Link href="/catalog">
+                <Button
+                  variant={"outline"}
+                  className="w-full justify-start gap-2"
+                >
+                  <ListOrderedIcon size={16} />
+                  Catálogo
+                </Button>
+              </Link>
+            </SheetClose>
           </div>
         </SheetContent>
       </Sheet>
